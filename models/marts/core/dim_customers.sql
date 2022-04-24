fe159e2c11d16b88b96e94594695080d920c5914
daniel.dominguez@baruchmail.cuny.edu <daniel.dominguez@baruchmail.cuny.edu>
2022-04-24 16:05:34 +0000

    DBT Tutorial Final

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,7 +10,7 @@ with customers as (
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 
 ),
 
@@ -21,7 +21,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
@@ -38,7 +39,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
@@ -47,3 +49,5 @@ final as (
 )
 
 select * from final
+
+order by customer_id
---
+++ models/staging/jaffle_shop/stripe.md
@@ -0,0 +1,12 @@
+{% docs paymentmethod %}
+	
+One of the following values: 
+
+| status         | definition                                       |
+|----------------|--------------------------------------------------|
+| bank transfer  | Wire transfer from a bank                        |
+| coupon         | Store issued coupon                              |
+| credit card    | Bank issued credit card                          |
+| gift card      | Store issued or AMEX gift card                   |
+
+{% enddocs %}
